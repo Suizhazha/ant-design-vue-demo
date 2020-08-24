142cfb72b0510095d2aeffdb5d1ffcf29d1f9bdc
Suizhazha <573505403@qq.com>
2020-08-24 10:28:21 +0800

    成功引入ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <a-form id="components-form-demo-normal-login"
+              :form="form"
+              class="login-form"
+              @submit="onSubmit"
+      >
+        <a-form-item>
+          <a-input
+              v-decorator="[
+          'userName',
+          { rules: [{required: true,message: '你没写用户名！！！'}] },
+        ]"
+              placeholder="Username"
+          >
+            <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+
+
+        <a-form-item>
+          <a-input
+              v-decorator="[
+          'password',
+          { rules: [{ required: true, message: '你没写密码！！！' }] },
+        ]"
+              type="password"
+              placeholder="Password"
+          >
+            <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+
+
+          <a-button type="primary" html-type="submit" class="login-form-button">
+            Log in
+          </a-button>
+
+      </a-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: 'normal_login' });
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+  },
+};
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#components-form-demo-normal-login .login-form {
+  max-width: 300px;
+}
+#components-form-demo-normal-login .login-form-forgot {
+  float: right;
+}
+#components-form-demo-normal-login .login-form-button {
+  width: 100%;
 }
 </style>
